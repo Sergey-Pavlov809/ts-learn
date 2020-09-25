0616ac032d322cb442cda49a399c4f0306a65fef
Sergey Pavlov <sergey-pavlov2610@mail.ru>
2020-09-25 04:57:13 +0300

    zxcasd

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 var User = /** @class */ (function () {
-    //Name: String;
-    //Age: number;
-    //constructor(name: string, age: number){
-    //    this.Name = name;
-    //    this.Age = age;
-    //}
     function User(name, age, weight) {
         this.name = name;
         this.age = age;
@@ -16,6 +10,44 @@ var User = /** @class */ (function () {
     return User;
 }());
 ;
-var el = document.getElementById('content'), names = ["zxc", "ASD", "qwe", "dfg"], users, names, map;
-(function (name) { return new User(name, 10, 50); });
-el.innerHTML = users.map(function (user) { return user.toString; }).reduce(function (prev, cur) { return prev + cur; }, '  ');
+var el = document.getElementById('content'), names = ['Alex', 'Tom', 'Dina', 'Emily', 'Andrew', 'Dora'], users = names.map(function (name) { return new User(name, Math.round(17.5 + Math.random() * 53), Math.round(49.5 + Math.random() * 51)); });
+el.innerHTML = users.map(function (user) { return user.toString(); })
+    .reduce(function (prev, cur) { return prev + cur; }, '');
+var opInp = document.createElement('form');
+opInp.setAttribute("id", "inpt");
+document.body.appendChild(opInp);
+var inp = document.createElement('input');
+inp.setAttribute("id", "inpVal");
+document.body.querySelector("form").appendChild(inp);
+var but1 = document.createElement('input');
+but1.setAttribute("type", "radio");
+but1.setAttribute("id", "btn1");
+but1.setAttribute("name", "rad");
+//but1.setAttribute("checked", "");
+document.body.querySelector("form").appendChild(but1);
+var but2 = document.createElement('input');
+but2.setAttribute("type", "radio");
+but2.setAttribute("value", "zxc");
+but2.setAttribute("name", "rad");
+but2.addEventListener("change", handlerBtn);
+document.body.querySelector("form").appendChild(but2);
+function handlerBtn(e) {
+    if (document.getElementById("btn1").checked = true) {
+        var filtredList = users.filter(function (user) { return user.age > document.getElementById("inpVal").value; });
+        var reduceList = filtredList.map(function (user) { return user.toString(); })
+            .reduce(function (prev, cur) { return prev + cur; }, '');
+        console.log(reduceList);
+        el.innerHTML = "" + reduceList;
+        console.log(document.getElementById("inpVal").value);
+        //document.body.appendChild(upul);
+    }
+    else {
+        var filtredList = users.filter(function (user) { return user.age < document.getElementById("inpVal").value; });
+        var reduceList = filtredList.map(function (user) { return user.toString(); })
+            .reduce(function (prev, cur) { return prev + cur; }, '');
+        console.log(reduceList);
+        el.innerHTML = "" + reduceList;
+        console.log(document.getElementById("inpVal").value);
+        //document.body.appendChild(upul);
+    }
+}
--- app.ts
+++ app.ts
@@ -1,13 +1,4 @@
-
-
 class User {
-    //Name: String;
-    //Age: number;
-
-    //constructor(name: string, age: number){
-    //    this.Name = name;
-    //    this.Age = age;
-    //}
 
     constructor(public name: string, public age: number,  public weight: number){}
 
@@ -16,9 +7,62 @@ class User {
     }
 };
 
+const el = document.getElementById( 'content' ),
+    names = ['Alex', 'Tom', 'Dina', 'Emily', 'Andrew', 'Dora'],
+    users = names.map( name => new User(name,
+        
+        Math.round( 17.5 + Math.random() * 53 ),
+        Math.round( 49.5 + Math.random() * 51 ) ) );
+        
+    
+el.innerHTML = users.map( user => user.toString() )
+        .reduce( (prev, cur) => prev + cur, '');
 
-const el = document.getElementById('content'),
-names = ["zxc","ASD","qwe","dfg"],
-users.names.map(name => new User(name,10,50))
-el.innerHTML = users.map(user => user.toString).reduce((prev, cur) => prev + cur,'  ')
+
+
+let opInp = document.createElement('form');
+opInp.setAttribute("id", "inpt")
+document.body.appendChild(opInp);
+
+
+let inp = document.createElement('input');
+inp.setAttribute("id", "inpVal");
+document.body.querySelector("form").appendChild(inp);
+
+
+let but1 = document.createElement('input');
+but1.setAttribute("type", "radio");
+but1.setAttribute("id", "btn1");
+but1.setAttribute("name", "rad");
+//but1.setAttribute("checked", "");
+document.body.querySelector("form").appendChild(but1);
+
+let but2 = document.createElement('input');
+but2.setAttribute("type", "radio");
+but2.setAttribute("value", "zxc");
+but2.setAttribute("name", "rad");
+but2.addEventListener("change",handlerBtn)
+document.body.querySelector("form").appendChild(but2);
+
+
+function handlerBtn(e){
+ if (document.getElementById("btn1").checked = true){
+    let filtredList = users.filter(user => user.age > document.getElementById("inpVal").value);
+    let reduceList = filtredList.map( user => user.toString() )
+        .reduce( (prev, cur) => prev + cur, '');
+    console.log(reduceList);
+    el.innerHTML = `${reduceList}`
+    console.log(document.getElementById("inpVal").value)
+        //document.body.appendChild(upul);
+    } else {
+        let filtredList = users.filter(user => user.age < document.getElementById("inpVal").value);
+        let reduceList = filtredList.map( user => user.toString() )
+            .reduce( (prev, cur) => prev + cur, '');
+        console.log(reduceList);
+        el.innerHTML = `${reduceList}`
+        console.log(document.getElementById("inpVal").value)
+            //document.body.appendChild(upul);
+    }
+    
+}
 
